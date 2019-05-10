35013c3da60c462b327b20cb195dc989bf4a1b58
Jude Juma <judejuma@gmail.com>
2019-05-10 15:02:15 +0300

    Adding constraints

--- multiples_of_Integer.py
+++ multiples_of_Integer.py
@@ -1,7 +1,9 @@
 def multiples(n):
-    '''takes n as integer and finds all first 10 multiples of n'''
-    for i in range (1,11):
-        result = n*i
-        print (n,'X', i, '=', result)
+    if (n < 20 and n > 2):
+        for i in range (1,11):
+            result = n*i
+            print (n,'X', i, '=', result)
+    else:
+        print ("The number is out of range")
 '''Calling the function with a sample integer, n =5'''
-multiples(5)
+multiples(6)
